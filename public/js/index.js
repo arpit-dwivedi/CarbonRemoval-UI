8e2536b431dbf4e831240b90cada17e9a1294bb9
ashishSingh <ashsihs41@gmail.com>
2022-06-01 21:01:26 +0530

    Updated Code

--- public/js/index.js
+++ public/js/index.js
@@ -48,43 +48,6 @@ function logout() {
     });
 }
 
-////loading of nft starts here
-//var itemsInMarketPlace = [];
-//var ref = firebase.database().ref('Company');
-
-//ref.once('value', function (snap) {
-//    snap.forEach(function (childData) {
-//        var item = {
-//            images: childData.val().image,
-//            latitude: childData.val().latitude,
-//            longitude: childData.val().longitude
-//        }
-//        itemsInMarketPlace.push(item);
-//    })
-//    var j = 0;
-//    for (var i = 0; i <= itemsInMarketPlace.length / 3; ++i) {
-//        let childDiv = "";
-//        let content = "";
-//        for (let x = 0; x < 3 && j < itemsInMarketPlace.length; ++x, ++j) {
-//            childDiv += `
-//                            <div class="w3-third img-thumbnail w3-container w3-margin-bottom"><img src="${itemsInMarketPlace[j].images}" alt="Norway" style="width:100%" class="w3-hover-opacity">
-//                                <div class="w3-container w3-white"">
-//                                    <p><strong>Carbon Emission : </strong></p>
-//                                    <span>Longitude: <strong>${itemsInMarketPlace[j].longitude} </strong></span>
-//                                    <span>Latitude: <strong>${itemsInMarketPlace[j].latitude} </strong></span>
-//                                    <div>
-//                                        <p style="float:left"><strong>Price : </strong></p>
-//                                         <button type="button" class="btn btn-primary buyNowNftIndex" style="float:right;margin-bottom:10px; font-size:12px;">Buy Now</button>
-//                                    </div>
-//                                 </div>
-//                            </div>`
-//        }
-//        content = `<div class="w3-row-padding">` + childDiv + `</div>`;
-//        document.getElementById('main-div').innerHTML += content;
-//    }
-//});
-
-
 const web3 = AlchemyWeb3.createAlchemyWeb3("https://eth-ropsten.alchemyapi.io/v2/qRiwHS9t7GVkOSDQJCXocuGu84EsYVwZ");
 
 const callContractForLoading = () => {
@@ -189,6 +152,10 @@ const loadMarketplaceData = (nft, nftAbi, nftContractAddress, marketPlace, marke
                                                 });
                                             });
                                     });
+                                }
+                                else {
+                                    document.getElementById('loaderDiv').hidden = true;
+                                    document.getElementById('textInfoDiv').hidden = false;
                                 }
 
                             });
